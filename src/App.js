2b193900e99b639a9e295ff278f83a8271fa757a
Filip Åhfelt <filahf94@gmail.com>
2020-05-31 19:43:58 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import './App.css';
-const { ipcRender } = window.require('electron');
+//const { ipcRender } = window.require('electron');
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <p>
-          Edit <code>src/Afilips</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className='App-link'
@@ -24,46 +24,3 @@ function App() {
 }
 
 export default App;
-
-/* desktopCapturer.getSources({ types: ['screen'] }).then(async (sources) => {
-  console.log(sources);
-  for (const source of sources) {
-    if (source.name === 'Screen 1') {
-      try {
-        navigator.mediaDevices
-          .getUserMedia({
-            audio: {
-              mandatory: {
-                chromeMediaSource: 'desktop',
-              },
-            },
-            video: false,
-          })
-          // eslint-disable-next-line no-loop-func
-          .then((mediaStream) => {
-            const recorder = new MediaRecorder(mediaStream);
-            var chunks = [];
-            recorder.onstart = (e) => {
-              chunks = [];
-            };
-            recorder.ondataavailable = (e) => {
-              chunks.push(e.data);
-            };
-            recorder.onstop = (e) => {
-              var blob = new Blob(chunks, { type: 'audio/mpeg3' });
-              socket.emit('radio', blob);
-            };
-
-            recorder.start();
-
-            setTimeout(function () {
-              recorder.stop();
-            }, 5000);
-          });
-      } catch (e) {
-        handleError(e);
-      }
-      return;
-    }
-  }
-}); */
